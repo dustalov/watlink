706459dc88d52c514b66bf1c335a3e188c14b7bd
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-04-12 03:13:22 +0500

    Update neighbors.py

--- exp/neighbors.py
+++ exp/neighbors.py
@@ -3,6 +3,7 @@
 import argparse
 from gensim.models.word2vec import Word2Vec
 import csv
+from collections import defaultdict
 import numpy as np
 import sys
 
@@ -23,9 +24,10 @@ w2v.init_sims(replace=True)
 print('Using %d word2vec dimensions from "%s".' % (w2v.layer1_size, args.w2v.name), flush=True, file=sys.stderr)
 
 index = faiss.IndexFlatIP(w2v.layer1_size)
+
 index.add(w2v.wv.syn0norm)
 
-neighbors = {}
+hyponyms = defaultdict(set)
 
 X = []
 
@@ -33,16 +35,19 @@ for row in csv.reader(args.subsumptions, delimiter='\t', quoting=csv.QUOTE_NONE)
     if len(row) < 2:
         continue
 
-    if row[1] not in w2v:
-        print('Skipping: (%s, %s)' % (row[0], row[1]), flush=True, file=sys.stderr)
+    print('\t'.join((row[0], row[1], '1')))
+
+    if row[0] not in w2v or row[1] not in w2v:
         continue
+
+    hyponyms[row[1]].add(row[0])
 
     X.append(w2v[row[1]])
 
-    if len(X) > 100:
-        break
-
 for D, I in zip(*index.search(np.array(X), args.k + 1)):
     for n, (d, i) in enumerate(zip(D.ravel(), I.ravel())):
+        hypernym = w2v.wv.index2word[i]
+
         if n > 0:
-            print(w2v.wv.index2word[i], d)
+            for hyponym in hyponyms[hypernym]:
+                print('\t'.join((hyponym, hypernym, str(d))))
