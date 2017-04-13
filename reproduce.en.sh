6b566ef8a43e4b1d7de5e802ed24dec730f40492
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-04-14 01:11:05 +0500

    New reproduce.*.sh

---
+++ reproduce.en.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+export LANG=en_US.UTF-8 LC_COLLATE=C
+
+rm -rfv eval/en
+
+for WEIGHT in tf idf tfidf; do
+
+mkdir -p eval/en/$WEIGHT
+
+for SYNSETS in data/en/*-synsets.tsv; do
+
+for ISAS in data/en/{patterns,wiktionary,joint}-isas.txt; do
+
+  if [ ! -f "$ISAS" ]; then
+    continue
+  fi
+
+  LINKED=$(basename "${SYNSETS%-synsets.tsv}")-$(basename "${ISAS%-isas.txt}")-linked.tsv
+  ./link.py --synsets=$SYNSETS --isas=$ISAS --weight=$WEIGHT | sort -t $'\t' -k2nr -k4nr -k1n -o "$LINKED"
+
+  ISAS=${LINKED%-linked.tsv}-isas.txt
+  ./linked-isas.awk "$LINKED" > "$ISAS"
+
+  mv -fv "$LINKED" "$ISAS" "eval/en/$WEIGHT"
+
+done
+
+done
+
+done
+
+eval/evaluate.py --gold=data/en/wordnet-isas.txt data/en/{patterns,wiktionary,joint}-isas.txt eval/en/**/*-isas.txt | tee pairwise-en-wordnet.tsv | sort -t $'\t' -g -k9r | column -t
---
+++ reproduce.ru.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+export LANG=en_US.UTF-8 LC_COLLATE=C
+
+rm -rfv eval/ru
+
+for WEIGHT in tf idf tfidf; do
+
+mkdir -p eval/ru/$WEIGHT
+
+for SYNSETS in data/ru/*-synsets.tsv; do
+
+for ISAS in data/ru/{patterns{,-limit,-filter},wiktionary,mas,joint}-isas.txt; do
+
+  if [ ! -f "$ISAS" ]; then
+    continue
+  fi
+
+  LINKED=$(basename "${SYNSETS%-synsets.tsv}")-$(basename "${ISAS%-isas.txt}")-linked.tsv
+  ./link.py --synsets=$SYNSETS --isas=$ISAS --weight=$WEIGHT | sort -t $'\t' -k2nr -k4nr -k1n -o "$LINKED"
+
+  ISAS=${LINKED%-linked.tsv}-isas.txt
+  ./linked-isas.awk "$LINKED" > "$ISAS"
+
+  mv -fv "$LINKED" "$ISAS" "eval/ru/$WEIGHT"
+
+done
+
+done
+
+done
+
+eval/evaluate.py --gold=data/ru/rwn-isas.txt data/ru/{patterns{,-limit,-filter},wiktionary,mas,joint}-isas.txt eval/ru/**/*-isas.txt | tee pairwise-ru-rwn.tsv | sort -t $'\t' -g -k9r | column -t
